6ac94d304a70a08ff63095de3d1a47b1af3893aa
Leif Andersen <leif@leifandersen.net>
2017-10-12 10:28:24 -0400

    Add info file.

---
+++ info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+(define collection "video-samples")
+(define deps '("base"
+               "rackunit-lib"
+               "draw-lib"
+               "gui-lib"
+               "pict-lib"
+               "video"))
+(define pkg-desc "Samples for Video")
+(define version "0.0")
+(define pkg-authors '(leif))
